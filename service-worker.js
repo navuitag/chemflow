400262819cebf910cd3668ca9b94d9a49f19a87b
Vu Nguyen <navuitag@gmail.com>
2026-06-12 22:59:08 +0700

    Add cross-app subject hub so learners can switch between courses from the navbar and home page.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "chemflow-vn-v18";
+const CACHE_NAME = "chemflow-vn-v19";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -15,6 +15,9 @@ const APP_SHELL = [
   "./assets/js/state.js",
   "./assets/js/profileStore.js",
   "./assets/js/studyTime.js",
+  "./assets/js/edtechApps.js",
+  "./components/edtechHub.js",
+  "./assets/css/edtech-hub.css",
   "./assets/js/utils.js",
   "./assets/js/chemFormat.js",
   "./vendor/three.module.min.js",
